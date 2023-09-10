47f470a0dbf471a423d3fdacc25e1fde3f0c2084
Sarasi Yashodha <sarasiyashodha67@gmail.com>
2023-09-10 20:14:21 +0530

    Starting the result page.

---
+++ lib/result_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+                right: 0,
+                child: Container(
+                  width: double.maxFinite,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage("Images/german_shepherd.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
